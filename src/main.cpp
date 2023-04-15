ef679af536fd3b7361f1291815762eeae6539bd5
JayXani <112290478+JayXani@users.noreply.github.com>
2023-04-15 00:19:45 -0300

    Troca para funções
    Alterei as variaveis para escopo global e criei funções para cada peça, para que quando fossemos criar as regras, as funções já estariam prontos.

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,17 @@ using namespace std;
         
 */
 
+//Variaveis de escopo global
+char peca;
+int podeMover = 0;
+int deslocaVertical;
+int deslocaHorizon;
+int linOrigem, colOrigem, linDestino, colDestino;
 
 /*Inicializando o tabuleiro, Q = queen, K = king
 Letras em maiúsculas representam peças Pretas, em minúculas peças brancas */
-
 char xadrez[10][10] = {
-	{'-', '1', '2', '3', '4', '5', '6', '7', '8' },    //TABULEIRO,  USEI UMA MATRIZ  10 X 10/
+	{'-', '1', '2', '3', '4', '5', '6', '7', '8' },
 	{'1', 't', 'c', 'b', 'k', 'q', 'b', 'c', 't' },
 	{'2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
 	{'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
@@ -34,12 +39,19 @@ char xadrez[10][10] = {
 	
 };
 
+//Declaração das funções no escopo
+int movimentoTorre();
+int movimentoBispo();
+int movimentoCavalo();
+int movimentoQueen();
+int movimentoKing();
+int movimentoPeao();
 
-//funÃ§Ã£o que printa o tabuleiro na tela
+//função que printa o tabuleiro na tela
 void printarMatriz(){
     char letra;
     for(int i = 0; i < 9; i++){
-        for(int pintaLinha = 0; pintaLinha < 3; pintaLinha++){ //Somente para deixar um espaÃ§o para a peÃ§a no tabuleiro e centralizar a peÃ§a
+        for(int pintaLinha = 0; pintaLinha < 3; pintaLinha++){ //Somente para deixar um espaço para a peça no tabuleiro e centraliza-la
             for(int j = 0; j < 9; j++){
             	
                 char peca = xadrez[i][j];
@@ -62,48 +74,45 @@ void printarMatriz(){
     }
 }
 
-//função que move as peças no tabuleiro
-
-//função que move as peças no tabuleiro
-int moverPecas(int linOrigem, int colOrigem, int linDestino, int colDestino){
-    char peca = xadrez[linOrigem][colOrigem];
-    int podeMover = 0;
-    int deslocaVertical = std::abs(linDestino - linOrigem);
-    int deslocaHorizon = std::abs(colDestino - colOrigem);
+//função que move TODAS as peças no tabuleiro
+int moverPecas(){
+    peca = xadrez[linOrigem][colOrigem];
+    deslocaVertical = std::abs(linDestino - linOrigem);
+    deslocaHorizon = std::abs(colDestino - colOrigem);
 	
 	switch(peca) {
-		//Se for uma torre, só poderá se mover se estiver na seguinte restrições
 		case 'T':
 			case 't':
-				podeMover = (deslocaVertical == 0 || deslocaHorizon == 0);
+				movimentoTorre();
 				break;
-		//Se for um bispo, só poderá se mover se estiver na seguinte restrições
+		
 		case 'B':
 			case 'b':
-			podeMover = (deslocaVertical == deslocaHorizon);
+                movimentoBispo();
 			break;
-		//Se for um cavalo, só poderá se mover se estiver na seguinte restrições
+
 		case 'C':
 			case 'c':
-				podeMover = ((deslocaVertical == 1 && deslocaHorizon == 2) || (deslocaVertical == 2 && deslocaHorizon == 1));
+				movimentoCavalo();
 				break;
-		//Se for uma rainha, só poderá se mover se estiver na seguinte restrições
+
 		case 'Q':
 			case 'q':
-				podeMover = (deslocaVertical == deslocaHorizon) || ((deslocaVertical == 0) || (deslocaHorizon == 0));
+				movimentoQueen();
 				break;
 		case 'K':
 			case 'k':
-				podeMover = (deslocaHorizon >= 1 || deslocaVertical <= 1);
+				movimentoKing();
 				break;
-		//Se for um peão, só poderá se mover se estiver na seguinte restrições
+		
 		case 'P':
-			podeMover = (linDestino - linOrigem == 1) && (deslocaHorizon == 1);
+			movimentoPeao();
 			break;	
 		case 'p':
-			podeMover = (linDestino - linOrigem == 1) && (deslocaHorizon == 0);
+			movimentoPeao();
 			break;
 	}
+
 	//Se a peça ela respeitar as restrições baseadas em seu tipo, ela poderá se mover (return 1)
 	if(podeMover) {
 		system("CLS");
@@ -116,19 +125,66 @@ int moverPecas(int linOrigem, int colOrigem, int linDestino, int colDestino){
 	}
 }
 
+//Função que analisa se os movimentos da TORRE são permitidos 
+int movimentoTorre(){
+
+    podeMover = (deslocaVertical == 0 || deslocaHorizon == 0);
+    return podeMover;
+     
+}
+
+//função que analisa se os movimentos do BISPO são permitidos
+int movimentoBispo(){
+    podeMover = (deslocaVertical == deslocaHorizon);
+    return podeMover;
+}
+
+//função que analisa se os movimentos do BISPO são permitidos
+int movimentoCavalo(){
+    podeMover = ((deslocaVertical == 1 && deslocaHorizon == 2) || (deslocaVertical == 2 && deslocaHorizon == 1));
+    return podeMover;
+}
+
+//função que analisa se os movimentos da RAINHA são permitidos
+int movimentoQueen(){
+    podeMover = (deslocaVertical == deslocaHorizon) || ((deslocaVertical == 0) || (deslocaHorizon == 0));
+    return podeMover;
+}
+
+//função que analisa se os movimentos do REI são permitidos
+int movimentoKing(){
+    podeMover = (deslocaHorizon >= 1 || deslocaVertical <= 1);
+    return podeMover;
+}
+
+//função que analisa se os movimentos do PEÃO são permitidos
+int movimentoPeao(){
+
+    if(peca == 'P'){
+        podeMover = ((linOrigem - linDestino == 1) && (deslocaHorizon == 0));
+        return podeMover;
+    }else{
+        if(peca == 'p'){
+            podeMover = ((linDestino - linOrigem == 1) && (deslocaHorizon == 0));
+            return podeMover;
+        }
+    }
+}
+
+
 int main (){
-    int linhaOrigem, linhaDestino, colunaOrigem, colunaDestino, resultado = 0;
+    int resultado = 0;
     while(1){
 
         printarMatriz();
 
         cout<<"Informe a linha e coluna de origem: "; 
-        scanf("%d %d", &linhaOrigem, &colunaOrigem);        //usei scanf porquê o "cin" está dando bug
+        scanf("%d %d", &linOrigem, &colOrigem);        //usei scanf porquê o "cin" está dando bug
 
         cout<<"Informe a linha e coluna de destino: ";
-        scanf("%d %d", &linhaDestino, &colunaDestino);
+        scanf("%d %d", &linDestino, &colDestino);
 
-        resultado = moverPecas(linhaOrigem, colunaOrigem, linhaDestino, colunaDestino);
+        resultado = moverPecas();
         if(resultado != 1){
             switch ( resultado )
             {
